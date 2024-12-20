b54a4f71c04d0f27f2c7e993a3a67661159e558f
Zorglube <630192+zorglube@users.noreply.github.com>
2024-12-20 15:35:42 +0100

    Update index.php

--- server/index.php
+++ server/index.php
@@ -108,6 +108,18 @@ function html_head() {
 		<link rel="stylesheet" type="text/css" href="style.css" />
 		<title>'.htmlspecialchars($title).'</title>
 		<link rel="icon" href="icon.svg" />
+  		<script type="text/JavaScript">
+			var _paq = window._paq = window._paq || [];
+			_paq.push([\'trackPageView\']);
+			_paq.push([\'enableLinkTracking\']);
+			(function() {
+				var u="https://qvfjshcweffsvuhxdjfd-matomo.services.clever-cloud.com/";
+				_paq.push([\'setTrackerUrl\', u+\'matomo.php\']);
+				_paq.push([\'setSiteId\', \'3\']);
+				var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
+				g.async=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);
+			})();
+		</script>
 	</head>
 	<body>
 		<h1>' . htmlspecialchars($title) . ' <img src="icon.svg" alt="" /></h1>
